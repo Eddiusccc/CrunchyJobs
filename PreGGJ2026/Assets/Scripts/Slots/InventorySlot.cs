4b5b7014f1f6d0d02e24387696da808867b8e64d
Raul Diaz <raukateromadness@gmail.com>
2026-01-23 09:09:59 -0400

    item and slots folder - Draggable and Inventory Slot script - rename scene and add new test scene - add pngs for new scene

---
+++ PreGGJ2026/Assets/Scripts/Slots/InventorySlot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        GameObject droppedObject = eventData.pointerDrag;
+        DraggableItem draggableItem = droppedObject.GetComponent<DraggableItem>();
+
+        if (draggableItem != null)
+        {
+            draggableItem.parentAfterDrag = transform;
+        } 
+    }
+
+}
