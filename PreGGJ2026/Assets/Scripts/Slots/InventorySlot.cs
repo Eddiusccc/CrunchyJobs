0e34ab99cb3e3830bf8820c06709074ce11995c1
Ed <sakealejandro23@gmail.com>
2026-01-23 20:36:37 -0400

    Apply DragSys with Job/Worker Sys
    la pocion representa al trabajador,
    el slot morado representa el job.
    el cuadro gris representa una casilla de descanso para el trabajador
    
    los datos salen en la consola, no hay resultado visual todavia

--- PreGGJ2026/Assets/Scripts/Slots/InventorySlot.cs
+++ PreGGJ2026/Assets/Scripts/Slots/InventorySlot.cs
@@ -3,16 +3,57 @@ using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    public bool isJobSlot = false;
+    Job job;
+
+    private void Start()
+    {
+        if (isJobSlot)
+        {
+            job = GetComponent<Job>();
+        }
+    }
+
+
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop");
         GameObject droppedObject = eventData.pointerDrag;
         DraggableItem draggableItem = droppedObject.GetComponent<DraggableItem>();
-
+        if(isJobSlot)
+        {
+            CheckWorker(droppedObject);
+        }
+        else
+        {
+            CheckWorkerUnassign(droppedObject);
+        }
         if (draggableItem != null)
         {
             draggableItem.parentAfterDrag = transform;
         } 
     }
 
+    void CheckWorker(GameObject go)
+    {
+        Worker worker = go.GetComponent<Worker>();
+        if(worker.currentState == WorkerState.Descansando)
+        {
+            if(job.jobState == JobState.Disponible)
+            {
+                job.AssignWorker(worker);
+                Debug.Log("Worker assigned to job: " + job.jobName);
+            }
+        }
+    }
+
+    void CheckWorkerUnassign(GameObject go)
+    {
+        Worker worker = go.GetComponent<Worker>();
+        if(worker.currentState == WorkerState.Trabajando)
+        {
+            Debug.Log("Worker unassigned from job: " + worker.currentJob.jobName);
+            worker.currentJob.RemoveWorker();
+        }
+    }
 }
